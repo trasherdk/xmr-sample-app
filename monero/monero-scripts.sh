d19a8ffb71f7734f93cceaaa66b47cf874b8e9ce
TrasherDK <trasherdk@fumlersoft.dk>
2020-03-06 03:24:30 +0100

    Add monero launch scripts

---
+++ monero/monero-scripts.sh
@@ -0,0 +1,135 @@
+NETTYPES="mainnet testnet stagenet"
+SESSIONS="xmr-app-daemon xmr-app-rpc"
+PARAMS="daemon wallet"
+
+is_running() {
+	local NAME="$1"
+
+	is_valid_session ${NAME} || return 1
+
+	local SNAME
+	for SNAME in $(screen -ls| grep -P '\d\.[a-zA-Z0-9\-]' | cut -f2 -d$'\t'|cut -f2 -d'.')
+	do
+		#echo "${WHITE}${SNAME} = ${NAME}${RESTORE}"
+		[ "${SNAME}" = "${NAME}-${NETTYPE}" ] \
+			&& {
+				echo "${GREEN}${SNAME}${WHITE} is running..${RESTORE}"
+				return 0
+			}
+	done
+
+  echo "${RED}${NAME}${WHITE} is not running..${RESTORE}"
+	return 1
+}
+
+is_valid_parameter() {
+
+	[[ ${PARAMS} =~ (^|[[:space:]])$1($|[[:space:]]) ]] \
+	|| {
+		echo "${RED} Invalid parameter name: ${YELLOW} $1 ${RESTORE}"
+		echo "${WHITE}Valid parameter names: ${YELLOW}${PARAMS}${RESTORE}"
+  	return 1
+	}
+
+	return 0
+}
+
+is_valid_nettype() {
+
+	[[ ${NETTYPES} =~ (^|[[:space:]])$1($|[[:space:]]) ]] \
+	|| {
+		echo "${RED} Invalid nettype name: ${YELLOW} $1 ${RESTORE}"
+		echo "${WHITE}Valid nettype names: ${YELLOW}${NETTYPES}${RESTORE}"
+  	return 1
+	}
+
+	return 0
+}
+
+is_valid_session() {
+
+	[[ ${SESSIONS} =~ (^|[[:space:]])$1($|[[:space:]]) ]] \
+	|| {
+		echo "${RED} Invalid session name: ${YELLOW} $1 ${RESTORE}"
+		echo "${WHITE}Valid session names: ${YELLOW}${SESSIONS}${RESTORE}"
+		return 1
+	}
+
+	return 0
+}
+
+stop_one() {
+	local SNAME=$1
+
+	is_valid_session ${SNAME} || return 1
+	is_running ${SNAME} || return 1
+	echo "${WHITE} Terminating ${YELLOW} ${SNAME} ${RESTORE}"
+	screen -X -S "${SNAME}-${NETTYPE}" quit
+
+}
+
+stop_daemon() {
+	local SNAME="xmr-app-daemon"
+	is_running ${SNAME} || return 1
+	echo "${WHITE} Terminating ${YELLOW} ${SNAME} ${RESTORE}"
+	screen -X -S "${SNAME}-${NETTYPE}" quit
+}
+
+stop_wallet() {
+	local SNAME="xmr-app-rpc"
+	is_running ${SNAME} || return 1
+	echo "${WHITE} Terminating ${YELLOW} ${SNAME} ${RESTORE}"
+	screen -X -S "${SNAME}-${NETTYPE}" quit
+}
+
+start_daemon() {
+	local SNAME="xmr-app-daemon"
+	is_running ${SNAME} && return 1
+
+	echo -n "${WHITE} Launching ${YELLOW} ${SNAME} : ${RESTORE}"
+
+	local DAEMON="${BIN}/monerod"
+	[ "${NETTYPE}" = "mainnet" ] || DAEMON="${DAEMON} --${NETTYPE}"
+
+	local CMD="${DAEMON} \
+		--prune-blockchain \
+		--data-dir ${DATA} \
+		--log-level 0 \
+		--log-file ${LOGS}/monerod.log \
+		--confirm-external-bind \
+		--rpc-bind-ip '86.48.96.142' \
+		--rpc-login superuser:abctesting123 \
+		--rpc-access-control-origins 'http://xmr-app.fumlersoft.dk,https://xmr-app.fumlersoft.dk,http://digest-request.fumlersoft.dk,https://digest-request.fumlersoft.dk'" 
+
+	screen -S "${SNAME}-${NETTYPE}" -d -m  bash -c "${CMD}" \
+	&& { echo ${GREEN}OK${RESTORE}; } \
+	|| { echo ${RED}FAIL${RESTORE}; } \
+}
+
+start_wallet() {
+	local SNAME="xmr-app-rpc"
+	is_running ${SNAME} && return 1
+
+	echo -n "${WHITE} Launching ${YELLOW} ${SNAME} : ${RESTORE}"
+
+	local RPC="${BIN}/monero-wallet-rpc"
+	[ "${NETTYPE}" = "mainnet" ] || RPC="${RPC} --${NETTYPE}"
+
+	local CMD="${RPC} \
+		--daemon-address 'http://xmr-app.fumlersoft.dk:38081' \
+		--daemon-login superuser:abctesting123 \
+		--log-level 4 \
+		--log-file ${LOGS}/monero-rpc.log \
+		--confirm-external-bind \
+		--rpc-bind-ip '86.48.96.142' \
+		--rpc-bind-port 38083 \
+		--wallet-dir ${WALLETS} \
+		--rpc-login rpc_user:abc123 \
+		--rpc-access-control-origins 'http://digest-request.fumlersoft.dk,https://digest-request.fumlersoft.dk,http://xmr-app.fumlersoft.dk,https://xmr-app.fumlersoft.dk'" 
+#		--disable-rpc-login \
+
+	screen -S "${SNAME}-${NETTYPE}" -d -m  bash -c "${CMD}" \
+	&& { echo ${GREEN}OK${RESTORE}; } \
+	|| { echo ${RED}FAIL${RESTORE}; } \
+
+}
---
+++ monero/monero.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+BASE=$(realpath "$(dirname $0)/..")
+
+NETTYPE="stagenet"
+DATA=/var/lib/monero/data/${NETTYPE}
+WALLETS=${DATA}/wallets
+LOGS=${DATA}/logs
+BIN=${BASE}/monero
+source ${BIN}/monero-scripts.sh
+source ${BIN}/colors.sh
+
+[ -d ${DATA} ] || mkdir -p ${DATA}
+[ -d ${WALLETS} ] || mkdir -p ${WALLETS}
+[ -d ${LOGS} ] || mkdir -p ${LOGS}
+
+echo "Network Type........: ${NETTYPE}"
+echo "Working directory...: ${BASE}"
+echo "Data directory......: ${DATA}"
+echo "Wallets directory...: ${WALLETS}"
+echo "Logs directory......: ${LOGS}"
+echo "Monero binaries.....: ${BIN}"
+
+[ -f ${BIN}/monerod ] \
+|| {
+  echo "This script expects to find monero binaries in:"
+  echo "\$BIN ${BIN}"
+  echo "If you have the monero binaries in another localion,"
+  echo "change the BIN variable to point to that location."
+  exit 1
+}
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    "ls")
+      shift
+      screen -ls | grep -i "xmr"
+      ;;
+    "stop")
+      shift
+      is_valid_parameter $1 || break
+      while [ $# -gt 0 ]
+      do
+        NAME=$1
+        case "${NAME}" in
+          "daemon")
+            stop_daemon
+            ;;
+          "wallet")
+            stop_wallet
+            ;;
+          *)
+            #echo "Unknown session: ${NAME}"
+            break
+            ;;
+        esac
+        shift
+      done
+      ;;
+    "start")
+      shift
+      is_valid_parameter $1 || break
+      while [ $# -gt 0 ]
+      do
+        NAME=$1
+        case "${NAME}" in
+          "daemon")
+            start_daemon
+            ;;
+          "wallet")
+            start_wallet
+            ;;
+          *)
+            #echo "Unknown session: ${NAME}"
+            break
+            ;;
+        esac
+        shift
+      done
+      ;;
+    "restart")
+      shift
+      is_valid_parameter $1 || break
+      while [ $# -gt 0 ]
+      do
+        NAME=$1
+        case "${NAME}" in
+          "daemon")
+            stop_daemon
+            start_daemon
+            ;;
+          "wallet")
+            stop_wallet
+            start_wallet
+            ;;
+          *)
+            #echo "Unknown session: ${NAME}"
+            break
+            ;;
+        esac
+        shift
+      done
+      ;;
+  esac
+done
+
+while [ $# -gt 0 ]
+do
+  echo "Unknown session: ${1}"
+  shift
+done
+
+screen -ls | grep 'xmr-app'
+
+exit
